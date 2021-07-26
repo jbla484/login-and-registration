77b9d75c7ced59073ed361a6e57882fd5a09bd74
James Blankenship <77518971+jbla484@users.noreply.github.com>
2021-07-25 20:53:45 -0700

    Update LoginAndRegistration.cpp

--- LoginAndRegistration.cpp
+++ LoginAndRegistration.cpp
@@ -3,11 +3,14 @@
 using namespace std;
 
 class User {
+//Public member functions
 public:
+    //Blank contructor
     User() {
 
     }
-    void resistering(string username, string password) {
+    //Registration function call to add input into array
+    void registering(string username, string password) {
         this->username = username;
         this->password = password;
 
@@ -16,6 +19,7 @@ public:
         accounts[count] = password;
         ++count;
     }
+    //Login function call to check if account exists
     void login(string username, string password) {
         count = 0;
         while (count <= 100) {
@@ -37,6 +41,7 @@ public:
             }
         }  
     }
+//Private data members
 private:
     string username;
     string password;
@@ -49,15 +54,17 @@ int main()
     string username, password;
     char response;
     User users;
-
+    //Intro message
     cout << "Welcome to James' login and registration program!" << endl;
     cout << "Enter \"q\" to close the program." << endl << endl;
-
+    //Looping until the user enters 'q'
     do {
         cout << "Login or Register? ( l / r ): " << endl;
         cin >> response;
-
+        //Switch statement for login and registration choices
         switch (response) {
+        //Login statement asks the user for their username and password and then 
+        //calls the login function with the username and password parameters
         case 'l':
             cout << "\nEnter your username: " << endl;
             cin >> username;
@@ -66,14 +73,17 @@ int main()
             users.login(username, password);
             cout << endl;
             break;
+        //Registering statement asks the user to enter a desired username and password and then 
+        //calls the registering function with the username and password parameters
         case 'r':
             cout << "\nEnter a username: " << endl;
             cin >> username;
             cout << "\nEnter a password: " << endl;
             cin >> password;
-            users.resistering(username, password);
+            users.registering(username, password);
             cout << "\nThanks for registering!" << endl << endl;
             break;
+        //Anything else other than 'l' or 'r'
         default:
             cout << "\nWrong choice, try again." << endl << endl;
             break;
